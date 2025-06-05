dc1360e9d829ebcb4ac030673ae020449e861573
krisnabimantoro <krisnabmntr@gmail.com>
2025-06-05 22:39:19 +0700

    Upgrade motion and framer-motion dependencies to version 12.16.0; add BlurFade, Pointer, and Lamp components for enhanced animations and user interaction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Spotlight } from "@/components/ui/spotlight";
 import { PointerHighlight } from "@/components/ui/pointer-highlight";
 import { div } from "motion/react-client";
+import { LampContainer } from "@/components/ui/lamp";
+import { motion } from "motion/react";
+import { BlurFade } from "@/components/magicui/blur-fade";
+import { Pointer } from "@/components/magicui/pointer";
 
 export default function Home() {
   return (
@@ -15,23 +20,45 @@ export default function Home() {
       />
 
       <Spotlight className=" fixed -top-40 left-0 md:-top-20 md:left-60 " fill="white" />
-      <div className="h-dvh flex items-center ">
-        <div className="relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
-          <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
-            Hi <br /> Thank You Was Coming in Here.
-          </h1>
-          <div className="mx-auto mt-4 max-w-lg text-base text-center font-bold tracking-tight md:text-base  text-neutral-300">
-            Do you want to know more
-            <PointerHighlight
-              rectangleClassName="bg-neutral-800 dark:bg-neutral-700 border-neutral-300 dark:border-neutral-600 leading-loose"
-              pointerClassName="text-yellow-500 h-3 w-3"
-              containerClassName="inline-block mx-1"
-            >
-              <span className="relative z-10"> about me?</span>
-            </PointerHighlight>
+      <section>
+        <div className="h-dvh flex items-center ">
+          <div className="relative z-10 mx-auto w-full max-w-7xl p-4 pt-20 md:pt-0">
+            <BlurFade delay={0.25 * 0.05} inView>
+              <h1 className="bg-opacity-50 bg-gradient-to-b from-neutral-50 to-neutral-400 bg-clip-text text-center text-4xl font-bold text-transparent md:text-7xl">
+                Helloo 👋
+                <br /> Thank You Was Coming in Here.
+              </h1>
+            </BlurFade>
+            <BlurFade delay={0.25} inView>
+              <div className="mx-auto mt-4 max-w-lg text-base text-center font-bold tracking-tight md:text-base  text-neutral-300">
+                Do you want to know more
+                <PointerHighlight
+                  rectangleClassName="bg-neutral-800 dark:bg-neutral-700 border-neutral-300 dark:border-neutral-600 leading-loose"
+                  pointerClassName="text-yellow-500 h-3 w-3"
+                  containerClassName="inline-block mx-1"
+                >
+                  <span className="relative z-10"> about me?</span>
+                </PointerHighlight>
+              </div>
+            </BlurFade>
           </div>
         </div>
-      </div>
+      </section>
+
+      {/* <LampContainer>
+        <motion.h1
+          initial={{ opacity: 0.5, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 0.3,
+            duration: 0.8,
+            ease: "easeInOut",
+          }}
+          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl "
+        >
+          Krisna Bimantoro
+        </motion.h1>
+      </LampContainer> */}
       {/* Example extra content to enable scrolling */}
       <div className="mt-20 space-y-10 w-[120vh]">
         <div className="h-96 bg-neutral-800/40 rounded-lg w-full">tes</div>
@@ -39,6 +66,7 @@ export default function Home() {
         <div className="h-96 bg-neutral-800/40 rounded-lg" />
         <div className="h-96 bg-neutral-800/40 rounded-lg" />
       </div>
+      {/* <Pointer className="fill-blue-500" /> */}
     </div>
   );
 }
